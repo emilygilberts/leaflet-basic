10a1c9dbc2273deee0d42cb146a0cf00c750a4a0
Your Name <you@example.com>
2024-08-15 15:46:56 +0200

    convert local kml file to geojson and add to map

--- src/App.vue
+++ src/App.vue
@@ -8,28 +8,67 @@
 import { onMounted } from "vue";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css"; // Import Leaflet CSS
+import { kml } from "@tmcw/togeojson";
 
+let layerControl;
 onMounted(() => {
   console.log("Initializing map...");
-
-  // Ensure that the #map container is available
-  const mapElement = document.getElementById("map");
-  if (mapElement) {
-    const map = L.map("map").setView([51.505, -0.09], 13);
-    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      maxZoom: 19,
-      attribution:
-        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    }).addTo(map);
-  } else {
-    console.error("Map container #map not found");
-  }
+  const map = setUpMap();
+  fetchAnddConvertLocalKMLToGeoJSON(map);
 });
+
+const fetchAnddConvertLocalKMLToGeoJSON = (map) => {
+  fetch("./half-dome.kml")
+    .then((response) => response.text())
+    .then((xml) => {
+      //convert xml to kml
+      const geojson = kml(new DOMParser().parseFromString(xml, "text/xml"));
+      console.log(geojson); // Log the converted GeoJSON
+      //create new overlay with geoJSON data
+      const geoJSONLayer = L.geoJSON(geojson);
+      //add the layer to the map directly
+      geoJSONLayer.addTo(map);
+      //add layer to layers control so that it can be toggled
+      layerControl.addOverlay(geoJSONLayer, "GeoJSON");
+      //add geojson layer to layercontrol
+    })
+    .catch((error) =>
+      console.error("Error fetching or processing KML file:", error)
+    );
+};
+
+const setUpMap = () => {
+  var osm = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution: "© OpenStreetMap",
+  });
+
+  var baseMaps = {
+    OpenStreetMap: osm,
+  };
+
+  var map = L.map("map", {
+    center: [39.73, -104.99],
+    zoom: 10,
+    layers: [osm],
+  });
+  layerControl = L.control.layers(baseMaps).addTo(map);
+  return map;
+};
 </script>
 
-<style scoped>
+<style>
+#app {
+  padding: 0;
+  margin: 0;
+  height: 100%;
+  width: 100%;
+}
+
 #map {
   height: 100vh; /* Adjust height as needed */
   width: 100vw; /* Ensure the map takes full width */
+  margin: 0;
+  padding: 0;
 }
 </style>
